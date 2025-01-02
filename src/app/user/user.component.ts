5e76d643d9489d8f97e87f55f092c339c55e6cea
Mamta Kumari <mamtakumari@Mamtas-MacBook-Pro.local>
2025-01-03 00:54:32 +0530

    attribute bindingreplaces get calling

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
 
-
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
@@ -11,5 +10,9 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-   selectedUser = DUMMY_USERS[randomIndex] ;
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagepath() {
+    return 'images/users/' + this.selectedUser.avatar;
+  }
 }
