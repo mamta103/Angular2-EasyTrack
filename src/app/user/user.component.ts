429412ea7946d4501f9c78b416b83a4f3816f305
Mamta Kumari <mamtakumari@Mamtas-MacBook-Pro.local>
2025-01-09 15:53:03 +0530

    Outputting List ContentFor Loop

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, output, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
-import { TasksComponent } from '../tasks/tasks.component';
+
+export interface User{
+  id: string;
+  avatar: string;
+  name: string;
+};
 
 @Component({
   selector: 'app-user',
@@ -9,23 +14,22 @@ import { TasksComponent } from '../tasks/tasks.component';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {
-  @Input({ required: true }) users!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
 
+
+
+export class UserComponent {
+   @Input({required:true}) users?: User;
   // @Input({ required: true }) id!: string;
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
+
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'images/users/' + this.users.avatar;
+    return 'images/users/' + this.users?.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.users.id);
+    this.select.emit(this.users?.id);
   }
 }
