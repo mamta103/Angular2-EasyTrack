878ef3837ece1624e21d095f55aa4707798d74fc
Mamta Kumari <mamtakumari@Mamtas-MacBook-Pro.local>
2025-01-09 23:12:53 +0530

    Outputting Conditional Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from '../dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +13,11 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = "u1"
+  selectedUserId?: string;
 
-get selectedUser(){
-  return this.users.find(user=> user.id === this.selectedUserId);
-}
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string) {
     this.selectedUserId = id;
