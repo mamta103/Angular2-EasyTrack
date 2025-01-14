8ca22596c466de465ba59e07137f98712486bcf9
Mamta Kumari <mamtakumari@Mamtas-MacBook-Pro.local>
2025-01-14 07:07:02 +0530

    Getting Started with Services

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-task',
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
